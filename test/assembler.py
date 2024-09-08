5bbd198af5730e7b9ee5070164ccf63f3b212f81
SIGMA <shoma@example.com>
2024-09-09 00:25:21 +0900

    R-typeのみの命令の出力とメモリを4バイト1行づつ出力できるようにした

--- test/assembler.py
+++ test/assembler.py
@@ -3,15 +3,16 @@ regs = []
 for i in range(32):
     regs.append(format(i, '05b'))
 
-OUTPUT_FILE = "text.txt"
+oneline = False
+OUTPUT_FILE = "./memory.txt"
 with open(OUTPUT_FILE, "w"):
     pass
 
 def main():
-    addi(1, 0, 5)
-    addi(2, 0, 8)
+    add(1, 0, 5)
+    add(2, 0, 8)
     add(3, 1, 2)
-    addi(4, 0, 2)
+    add(4, 0, 2)
     sub(5, 3, 4)
 
 
@@ -19,26 +20,26 @@ def lui(dreg, imm):
     simm = (imm << 12) & 4294967295
     fimm = format(simm, '020b')
     inst = fimm + regs[dreg] + "0110111"
-    output(inst, OUTPUT_FILE)
+    output(inst, OUTPUT_FILE, oneline)
 
 def add(rd, rs1, rs2):
     inst = "0000000" + regs[rs2] + regs[rs1] + "000" + regs[rd] + "0110011"
-    output(inst, OUTPUT_FILE)
+    output(inst, OUTPUT_FILE, oneline)
 
 def sub(rd, rs1, rs2):
     inst = "0100000" + regs[rs2] + regs[rs1] + "000" + regs[rd] + "0110011"
-    output(inst, OUTPUT_FILE)
+    output(inst, OUTPUT_FILE, oneline)
 
 def xor(rd, rs1, rs2):
     inst = "0000000" + regs[rs2] + regs[rs1] + "100" + regs[rd] + "0110011"
-    output(inst, OUTPUT_FILE)
+    output(inst, OUTPUT_FILE, oneline)
 
 def addi(rd, rs1, imm):
     fimm = format(imm & 4095, '012b')
     inst = fimm + regs[rs1] + "000" + regs[rd] + "0010011"
-    output(inst, OUTPUT_FILE)
+    output(inst, OUTPUT_FILE, oneline)
 
-def output(inst, filename):
+def output(inst, filename, oneline=False):
     inst_int = int(inst, 2)
     inst_hex = format(inst_int, '08x')
     print(inst)
@@ -51,8 +52,11 @@ def output(inst, filename):
     print(byte)
 
     with open(filename, "a") as f:
-        for b in byte[::-1]:
-            f.write(b + '\n')
+        if oneline:
+            f.write("".join(byte[::-1]) + '\n')
+        else:
+            for b in byte[::-1]:
+                f.write(b + '\n')
     
 
 
