3ed9c348ffd2168cd205686ef54056cc6cb64529
SIGMA <shoma@example.com>
2024-09-10 07:19:30 +0900

    ALUにand, or, xor演算を実装

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -5,7 +5,7 @@ module ALU(input_a, input_b, ctrl, is_zero, out);
     output is_zero;
     output[31:0] out;
 
-    wire[31:0] output_adder, output_sub;
+    wire[31:0] output_adder, output_sub, output_and, output_or, output_xor;
 
     adder32 adder(
         .a(input_a),
@@ -20,7 +20,14 @@ module ALU(input_a, input_b, ctrl, is_zero, out);
         .sub(output_sub)
     );
 
+    assign output_and = input_a & input_b;
+    assign output_or  = input_a | input_b;
+    assign output_xor = input_a ^ input_b;
+
     assign out = (ctrl == 4'b0000)? output_adder:
                  (ctrl == 4'b1000)? output_sub:
+                 (ctrl == 4'b0100)? output_xor:
+                 (ctrl == 4'b0110)? output_or:
+                 (ctrl == 4'b0111)? output_and:
                                     32'h00000000;
 endmodule
--- ALU/tb_ALU.v
+++ ALU/tb_ALU.v
@@ -28,15 +28,15 @@ module tb_ALU();
         
         #10 input_a = 32'h00000004;
             input_b = 32'h00000008;
-            ctrl = 4'b1000;
+            ctrl = 4'b0100;
 
         #10 input_a = 32'h0000ffff;
             input_b = 32'h00000001;
-            ctrl = 4'b1000;
+            ctrl = 4'b0100;
 
         #10 input_a = 32'hffffffff;
-            input_b = 32'h00000001;
-            ctrl = 4'b1000;
+            input_b = 32'hffff0001;
+            ctrl = 4'b0100;
         
     end
 endmodule
