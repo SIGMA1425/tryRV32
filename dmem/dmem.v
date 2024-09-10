adede635060988033f9a026373f096f0407970da
SIGMA <shoma@example.com>
2024-09-11 01:37:09 +0900

    dmemのテストを実施．CLK信号の入力定義と結線ができていたかったため修正

--- dmem/dmem.v
+++ dmem/dmem.v
@@ -1,12 +1,12 @@
 module dmem(CLK, addr, w_data, ctrl, w_en, outdata);
     input[31:0] addr, w_data;
     input[2:0] ctrl;
-    input w_en;
+    input w_en, CLK;
     reg[7:0] data[0:4096];
     output[31:0] outdata;
 
     always @(posedge CLK) begin
-        if (w_en == 1'b0) begin
+        if (w_en == 1'b1) begin
             if (ctrl == 3'b000) begin
                 data[addr] <= w_data[7:0];
             end
@@ -15,6 +15,7 @@ module dmem(CLK, addr, w_data, ctrl, w_en, outdata);
                 data[addr + 1] <= w_data[15:8];
             end
             else if(ctrl == 3'b010) begin
+                $display("OK");
                 data[addr] <= w_data[7:0];
                 data[addr + 1] <= w_data[15:8];
                 data[addr + 2] <= w_data[23:16];
--- dmem/tb_mem.v
+++ dmem/tb_mem.v
@@ -2,11 +2,12 @@
 module tb_dmem();
     reg[31:0] addr, w_data;
     reg[2:0] ctrl;
-    reg w_en;
-    wire outdata;
+    reg w_en, CLK;
+    wire[31:0] outdata;
 
     parameter RATE = 100;
     dmem dmem(
+        .CLK(CLK),
         .addr(addr),
         .w_data(w_data),
         .ctrl(ctrl),
@@ -14,20 +15,47 @@ module tb_dmem();
         .outdata(outdata)
     );
 
-    always @ * (RATE / 2) CLK = ~CLK;
+    always #(RATE/2) CLK = ~CLK;
 
     initial begin
         $dumpfile("tb_dmem.vcd");
         $dumpvars(0, dmem);
-        $monitor("CLK = %d, w_en = %d, addr = %x, w_data = %x, ctrl = %b, outdata = %x", CLK, w_en, addr, w_data, ctrl, outdata);
+        $monitor("CLK = %d, w_en = %d, addr = %x, w_data = %x, ctrl = %b, outdata = %x", CLK, w_en, addr, w_data, dmem.ctrl, outdata);
     end
 
     initial begin
         #0  addr = 32'h00000000;
             w_data = 32'h00000000;
-            ctrl = 3'b000;
+            ctrl = 3'b010;
             w_en = 1'b0;
+            CLK = 1'b0;
         
         #10 w_en = 1'b1;
+            w_data = 32'h12345678;
+            ctrl = 3'b010;
+
+        #RATE w_en = 1'b0;
+              ctrl = 3'b000;
+        
+        #RATE ctrl = 3'b001;
+
+        #RATE ctrl = 3'b010;
+        #RATE ctrl = 3'b100;
+        #RATE ctrl = 3'b101;
+
+        #10 w_en = 1'b1;
+            w_data = 32'hf123f6f8;
+            ctrl = 3'b001;
+
+        #RATE w_en = 1'b0;
+              ctrl = 3'b000;
+        
+        #RATE ctrl = 3'b001;
+
+        #RATE ctrl = 3'b010;
+        #RATE ctrl = 3'b100;
+        #RATE ctrl = 3'b101;
+
+        #(RATE * 10) $finish;
     end
 endmodule
